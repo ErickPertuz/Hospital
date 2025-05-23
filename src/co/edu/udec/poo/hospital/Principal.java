cc1d8ed16783e23d6b4dcf688f3f75452191911a
Erick Pertuz Anaya <>
2025-05-23 03:09:54 -0500

    Creación de la clase con el metodo main e inicio del diagramas de clase conceptual

---
+++ src/co/edu/udec/poo/hospital/Principal.java
@@ -0,0 +1,16 @@
+package co.edu.udec.poo.hospital;
+
+/**
+ *
+ * @author DANI
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
